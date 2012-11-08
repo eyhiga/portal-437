01078f524314beb8cd21d95a91b7dda4654992c6
eyhiga@gmail.com <eyhiga@gmail.com@9491312f-cf03-43af-e11a-aa496f1b96f2>
2012-11-08 01:51:53 +0000

    Adicionando parte inicial de produtos

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once "header.php";
 
 <br/><a href="index.php">Home</a>
 <br/><a href="carrinho.php">Carrinho de Compras</a>
-<br/><a href="produtos.php">Produtos</a>
+<br/><a href="categorias.php">Categorias</a>
 <br/><a href="finalizarCompra.php">Finalizar Compra</a>
 
 <?php 
