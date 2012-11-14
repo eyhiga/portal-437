b3387d23913eb57120a4ee835a8eb1a930269510
dmreis8@gmail.com <dmreis8@gmail.com@9491312f-cf03-43af-e11a-aa496f1b96f2>
2012-11-14 01:08:46 +0000

    Fluxos

--- carrinho.php
+++ carrinho.php
@@ -5,7 +5,11 @@ require_once "header.php";
 
 ?>
 
-Carrinho de Compras
+Carrinho de Compras<br/>
+RUDÁ: Aqui, mostrar uma lista com todos os itens do carrinho de compras do usuário, com respectiva quantidade de cada e valor, e também valor total.
+Guardar dados na session<br/>
+
+<a href="finalizarCompra.php"><input type="button" id="botaoFinalizarCompra" value="Finalizar Compra" /></a>
 
 <?php 
 
