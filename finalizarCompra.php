803d1f7637521ba61d9a7300e77d1b15d98415b6
eyhiga@gmail.com <eyhiga@gmail.com@9491312f-cf03-43af-e11a-aa496f1b96f2>
2012-11-15 18:04:15 +0000

    Algumas partes

--- finalizarCompra.php
+++ finalizarCompra.php
@@ -19,22 +19,50 @@ Eduardo - Verificar se cliente é bom pagador (Grupo 04);<br/>
                    "token" => "0123456789");
     
     $client4 = new nusoap_client($comp04, true);
-    $score = $client4->call("getScore", $args4);
-    print_r($score);
+    $client4_resp = $client4->call("getScore", $args4);
+    $score = $client4_resp["return"]["score"];
+    echo "Score:".$score."<br/>";
+    //print_r($score);
 ?>
 
 Eduardo - Verificar se tem produto no estoque (Grupo 08);<br/>
 
 <?php
-    $link8 = $comp08."10".".json";
-    $ret8 = file_get_contents($link8);
-    echo $link8;
-    echo $ret8;
+    $link8Qtd = $comp08Qtd."10".".xml";
+    $ret8Qtd = file_get_contents($link8Qtd);
+    $ret8Qtd = file_get_contents("http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/currentQuantity/10.json");
+
+    //echo $link8Qtd;
+    echo $ret8Qtd;
     //$qts8 = json_decode($ret8);
     //print_r($qts8);
+    echo "<br/>";
 ?>
 
-Eduardo - Dar baixa no estoque (Grupo 08) e cadastrar entrega (Grupo 09).<br/><br/><br/>
+Eduardo - Dar baixa no estoque (Grupo 08)
+
+<?php
+
+    $link08Upd = $comp08Upd;
+    $attr08Upd = array("code" => "10",
+                       "quantity" => "10");
+    $params08Upd = array("http" => array(
+                                    "method" => "PUT",
+                                    "header" => "Content-type: text/xml",
+                                    "content" => $attr08Upd));
+    
+    $response08Upd = file_get_contents($link08Upd, false, $params08Upd);
+    print_r($response08Upd);
+
+?>
+<br/>
+cadastrar entrega (Grupo 09).<br/><br/><br/>
+
+<?php
+
+    
+
+?>
 
 <?php 
 
