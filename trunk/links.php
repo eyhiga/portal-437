0f5be309298566df85e950b5b575f87a4f8be8fe
eyhiga@gmail.com <eyhiga@gmail.com@9491312f-cf03-43af-e11a-aa496f1b96f2>
2012-11-15 18:04:15 +0000

    Algumas partes

--- trunk/links.php
+++ trunk/links.php
@@ -1,4 +1,5 @@
 <?php 
     $comp04 = "http://staff01.lab.ic.unicamp.br:8480/ModuloValidacaoCreditoWS/services/ValidacaoCreditoService?wsdl";
-    $comp08 = "http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/currentQuantity/";
+    $comp08Qtd = "http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/currentQuantity/";
+    $comp08Upd = "http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/quantity/";
 ?>
