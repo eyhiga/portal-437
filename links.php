f5cfd28df9f4b215e41afba4a4cbe3b804df9a9b
dmreis8@gmail.com <dmreis8@gmail.com@9491312f-cf03-43af-e11a-aa496f1b96f2>
2012-11-20 18:20:01 +0000

    Fluxo de compra: escolha de endereço de entrega, frete, meio de pagamento

--- links.php
+++ links.php
@@ -1,6 +1,18 @@
 <?php 
+	/* Busca de endereço por CEP */
+	// https://docs.google.com/spreadsheet/ccc?key=0AnXiYPrlU4dtdGhKTXA3Z3NuOGlrWGt3dzM1TDIwWFE#gid=0
+	// http://g2mc437.heliohost.org/parte2/service/teste-cep.php
+	$comp02 = "http://g2mc437.heliohost.org/parte2/service/webserver.php/g02_busca_por_cep?wsdl";
+	
+	//
     $comp04 = "http://staff01.lab.ic.unicamp.br:8480/ModuloValidacaoCreditoWS/services/ValidacaoCreditoService?wsdl";
+    
+    //
     $comp08Qtd = "http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/currentQuantity/";
+    
+    //
     $comp08Upd = "http://g6:g6@mc437-g8-estoque-v2.webbyapp.com/products/quantity/";
+    
+    // Escolha de Frete
     $comp09 = "http://staff01.lab.ic.unicamp.br/grupo9/webservice/ws.php?wsdl";
 ?>
